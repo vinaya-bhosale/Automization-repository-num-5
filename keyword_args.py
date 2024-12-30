160111d4951a2f34fdde5df3c1a766f484a4d0dd
Vinaya-bhosale <vinayabhosale25@gmail.com>
2024-12-30 08:19:10 -0800

    this is keyword argument program

---
+++ keyword_args.py
@@ -0,0 +1,10 @@
+class A:
+    def fun(self, name, roll, mob, addr):
+        print("student name:", name)
+        print("student roll_num ", roll)
+        print("student mobil is ", mob)
+        print("student addr is: ", addr)
+
+
+o = A()
+o.fun(name="vinaya", roll=21, mob=1234567898, addr="pune")
