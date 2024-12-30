3a7733568c300d31c60c1d24848c0811ba269e6b
Vinaya-bhosale <vinayabhosale25@gmail.com>
2024-12-30 08:37:59 -0800

    this is positional argument peogram

---
+++ positional_args.py
@@ -0,0 +1,10 @@
+class A:
+    def stud(self, name, roll, mob, address):
+        print("student namei", name)
+        print("roll num is", roll)
+        print("mobile num", mob)
+        print("student address is ", address)
+
+
+obj = A()
+obj.stud("vinaya", 21, 1234456789, 'pune')
